dcf25d289ab8ac61f6e0841eb15170a8f7605a2a
krisna-sh <krishnakorata04@gmail.com>
2026-02-25 16:01:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert("hello world");
+function endtheworld() {
+  const mainTextElement = document.getElementById("main-text");
+  if(mainTextElement.innerText === "By by world")
+  {
+    mainTextElement.innerText = "helloo world";
+  }
+  else
+  {
+    mainTextElement.innerText = "by by world";
+  }
+}
